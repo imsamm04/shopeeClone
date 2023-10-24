a68b2676e0339df088e7f8f366e10088b1ef597e
viet.dinhduc <ducviet2504@gmail.com>
2023-10-24 09:49:56 +0700

    updpate date select

--- src/pages/User/components/UserSlideNav/UserSideNav.tsx
+++ src/pages/User/components/UserSlideNav/UserSideNav.tsx
@@ -1,9 +1,10 @@
 import React, { useContext } from 'react'
-import { Link } from 'react-router-dom'
+import { Link, NavLink } from 'react-router-dom'
 import path from 'src/constants/path'
 import { AppContext } from 'src/context/app.context'
 import userImage from 'src/assets/images/user.svg'
 import { getAvatarUrl } from 'src/utils/utils'
+import classNames from 'classnames'
 export default function UserSideNav() {
   const { profile } = useContext(AppContext)
   return (
@@ -33,32 +34,48 @@ export default function UserSideNav() {
         </div>
       </div>
       <div className='mt-7'>
-        <Link to={path.profile} className='flex items-center capitalize text-orange transition-colors'>
+        <NavLink
+          to={path.profile}
+          className={({ isActive }) =>
+            classNames('flex items-center capitalize  transition-colors', {
+              'text-orange': isActive,
+              'text-gray-600': !isActive
+            })
+          }
+        >
           <div className='mr-3 h-[22px] w-[22px]'>
-            <img alt='' src={profile?.avatar || userImage} className='h-full w-full' />
+            <img src='https://cf.shopee.vn/file/ba61750a46794d8847c3f463c5e71cc4' alt='' className='h-full w-full' />
           </div>
           Tài khoản của tôi
-        </Link>
-        <Link to={path.changePassword} className='mt-4 flex items-center capitalize text-gray-600 transition-colors'>
+        </NavLink>
+        <NavLink
+          to={path.changePassword}
+          className={({ isActive }) =>
+            classNames('mt-4 flex items-center capitalize transition-colors', {
+              'text-orange': isActive,
+              'text-gray-600': !isActive
+            })
+          }
+        >
           <div className='mr-3 h-[22px] w-[22px]'>
-            <img
-              alt=''
-              src='https://down-vn.img.susercontent.com/file/ba61750a46794d8847c3f463c5e71cc4'
-              className='h-full w-full'
-            />
+            <img src='https://cf.shopee.vn/file/ba61750a46794d8847c3f463c5e71cc4' alt='' className='h-full w-full' />
           </div>
           Đổi mật khẩu
-        </Link>
-        <Link to={path.historyPurchase} className=' text-grey mt-4 flex items-center capitalize transition-colors'>
+        </NavLink>
+        <NavLink
+          to={path.historyPurchase}
+          className={({ isActive }) =>
+            classNames('mt-4 flex items-center  capitalize transition-colors', {
+              'text-orange': isActive,
+              'text-gray-600': !isActive
+            })
+          }
+        >
           <div className='mr-3 h-[22px] w-[22px]'>
-            <img
-              alt=''
-              className='h-full w-full'
-              src='https://down-vn.img.susercontent.com/file/f0049e9df4e536bc3e7f140d071e9078'
-            ></img>
+            <img src='https://cf.shopee.vn/file/f0049e9df4e536bc3e7f140d071e9078' alt='' className='h-full w-full' />
           </div>
           Đơn mua
-        </Link>
+        </NavLink>
       </div>
     </div>
   )
