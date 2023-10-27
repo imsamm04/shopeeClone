f3bf7103a110a5b9a8b82ff7d3918fd97d554082
viet.dinhduc <ducviet2504@gmail.com>
2023-10-27 15:33:24 +0700

    add 404 not found page

--- src/useRouteElement.tsx
+++ src/useRouteElement.tsx
@@ -14,6 +14,7 @@ import CartLayout from './layouts/CartLayout'
 import UserLayout from './pages/User/UserLayout'
 import ChangePassword from './pages/User/Pages/ChangePassword'
 import HistoryPurchase from './pages/User/Pages/HistoryPurchase'
+import NotFound from './pages/NotFound'
 
 function ProtectedRoute() {
   const { isAuthenticated } = useContext(AppContext)
@@ -33,6 +34,14 @@ export default function useRouteElements() {
       element: (
         <MainLayout>
           <ProductList />
+        </MainLayout>
+      )
+    },
+    {
+      path: '*',
+      element: (
+        <MainLayout>
+          <NotFound />
         </MainLayout>
       )
     },
